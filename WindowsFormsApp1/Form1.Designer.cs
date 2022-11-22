b7eb0b919c5146937ffe3578d1f6682778e78969
gavrilovem <artov.2082@mail.ru>
2022-11-22 17:39:07 +0300

    deleted:    Form1.Designer.cs new file:   WindowsFormsApp1.sln renamed:    App.config -> WindowsFormsApp1/App.config new file:   WindowsFormsApp1/DekstraAlgorim.cs new file:   WindowsFormsApp1/Form1.Designer.cs renamed:    Form1.cs -> WindowsFormsApp1/Form1.cs renamed:    Form1.resx -> WindowsFormsApp1/Form1.resx renamed:    Program.cs -> WindowsFormsApp1/Program.cs renamed:    Properties/AssemblyInfo.cs -> WindowsFormsApp1/Properties/AssemblyInfo.cs renamed:    Properties/Resources.Designer.cs -> WindowsFormsApp1/Properties/Resources.Designer.cs renamed:    Properties/Resources.resx -> WindowsFormsApp1/Properties/Resources.resx renamed:    Properties/Settings.Designer.cs -> WindowsFormsApp1/Properties/Settings.Designer.cs renamed:    Properties/Settings.settings -> WindowsFormsApp1/Properties/Settings.settings renamed:    WindowsFormsApp1.csproj -> WindowsFormsApp1/WindowsFormsApp1.csproj new file:   WindowsFormsApp1/bin/Debug/WindowsFormsApp1.exe renamed:    bin/Debug/WindowsFormsApp1.exe.config -> WindowsFormsApp1/bin/Debug/WindowsFormsApp1.exe.config new file:   WindowsFormsApp1/bin/Debug/WindowsFormsApp1.pdb new file:   WindowsFormsApp1/bin/Debug/stores/edge.txt new file:   WindowsFormsApp1/bin/Debug/stores/vertex.txt renamed:    bin/Debug/storesedge.txt -> WindowsFormsApp1/bin/Debug/storesedge.txt renamed:    bin/Debug/storesvertex.txt -> WindowsFormsApp1/bin/Debug/storesvertex.txt renamed:    obj/Debug/.NETFramework,Version=v4.7.2.AssemblyAttributes.cs -> WindowsFormsApp1/obj/Debug/.NETFramework,Version=v4.7.2.AssemblyAttributes.cs renamed:    obj/Debug/DesignTimeResolveAssemblyReferences.cache -> WindowsFormsApp1/obj/Debug/DesignTimeResolveAssemblyReferences.cache renamed:    obj/Debug/DesignTimeResolveAssemblyReferencesInput.cache -> WindowsFormsApp1/obj/Debug/DesignTimeResolveAssemblyReferencesInput.cache renamed:    obj/Debug/WindowsFormsApp1.Form1.resources -> WindowsFormsApp1/obj/Debug/WindowsFormsApp1.Form1.resources renamed:    obj/Debug/WindowsFormsApp1.Properties.Resources.resources -> WindowsFormsApp1/obj/Debug/WindowsFormsApp1.Properties.Resources.resources renamed:    obj/Release/WindowsFormsApp1.csproj.AssemblyReference.cache -> WindowsFormsApp1/obj/Debug/WindowsFormsApp1.csproj.AssemblyReference.cache new file:   WindowsFormsApp1/obj/Debug/WindowsFormsApp1.csproj.CoreCompileInputs.cache renamed:    obj/Debug/WindowsFormsApp1.csproj.FileListAbsolute.txt -> WindowsFormsApp1/obj/Debug/WindowsFormsApp1.csproj.FileListAbsolute.txt new file:   WindowsFormsApp1/obj/Debug/WindowsFormsApp1.csproj.GenerateResource.cache renamed:    obj/Debug/WindowsFormsApp1.csproj.SuggestedBindingRedirects.cache -> WindowsFormsApp1/obj/Debug/WindowsFormsApp1.csproj.SuggestedBindingRedirects.cache new file:   WindowsFormsApp1/obj/Debug/WindowsFormsApp1.exe new file:   WindowsFormsApp1/obj/Debug/WindowsFormsApp1.pdb renamed:    obj/Release/.NETFramework,Version=v4.7.2.AssemblyAttributes.cs -> WindowsFormsApp1/obj/Release/.NETFramework,Version=v4.7.2.AssemblyAttributes.cs renamed:    obj/Debug/WindowsFormsApp1.csproj.AssemblyReference.cache -> WindowsFormsApp1/obj/Release/WindowsFormsApp1.csproj.AssemblyReference.cache deleted:    bin/Debug/WindowsFormsApp1.exe deleted:    bin/Debug/WindowsFormsApp1.pdb deleted:    bin/Debug/stores/edge.txt deleted:    bin/Debug/stores/vertex.txt deleted:    obj/Debug/WindowsFormsApp1.csproj.CoreCompileInputs.cache deleted:    obj/Debug/WindowsFormsApp1.csproj.GenerateResource.cache deleted:    obj/Debug/WindowsFormsApp1.exe deleted:    obj/Debug/WindowsFormsApp1.pdb

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.eventLog1 = new System.Diagnostics.EventLog();
+            this.directorySearcher1 = new System.DirectoryServices.DirectorySearcher();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Control;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.Location = new System.Drawing.Point(900, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Add Node";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.Control;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.Location = new System.Drawing.Point(999, 24);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(118, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Remove Node";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(869, 483);
+            this.panel1.TabIndex = 4;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.Control;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.Location = new System.Drawing.Point(1000, 62);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(117, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Remove Edge";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.Control;
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.Location = new System.Drawing.Point(900, 62);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(93, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Add Edge";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(900, 159);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(93, 22);
+            this.numericUpDown1.TabIndex = 7;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(900, 199);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(93, 22);
+            this.numericUpDown2.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(897, 129);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 16);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Кратчайший путь";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(900, 228);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "Найти";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // eventLog1
+            // 
+            this.eventLog1.SynchronizingObject = this;
+            // 
+            // directorySearcher1
+            // 
+            this.directorySearcher1.ClientTimeout = System.TimeSpan.Parse("-00:00:01");
+            this.directorySearcher1.ServerPageTimeLimit = System.TimeSpan.Parse("-00:00:01");
+            this.directorySearcher1.ServerTimeLimit = System.TimeSpan.Parse("-00:00:01");
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(900, 258);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 16);
+            this.label2.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1126, 507);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button5;
+        private System.Diagnostics.EventLog eventLog1;
+        private System.DirectoryServices.DirectorySearcher directorySearcher1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
